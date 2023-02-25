9afb08400d5833e9e5f60dcfea470d5f835785a5
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-02-25 22:05:07 +0400

    update

--- script.js
+++ script.js
@@ -3,8 +3,36 @@
 //////////////////////////////////
 const btnElement = document.getElementById("btn");
 
+function createNoteElement(id, content) {
+  const element = document.createElement("textarea");
+  element.classList.add("note");
+  element.placeholder = "Empty Note";
+  element.value = content;
+
+  element.addEventListener("dblclick", () => {
+    const warning = confirm("Do you want to delete this note?");
+    if (warning) {
+      deleteNote(id, element);
+    }
+  });
+
+  element.addEventListener("input", () => {
+    updateNote(id, element.value);
+  });
+}
+
+function deleteNote() {
+  console.log(555);
+}
+
+function updateNote() {}
+
 function addNote() {
-  console.log("hello");
+  const noteObject = {
+    id: Math.floor(Math.random() * 100000),
+    content: "",
+  };
+  const noteElement = createNoteElement(noteObject.id, noteObject.content);
 }
 
 btnElement.addEventListener("click", addNote);
